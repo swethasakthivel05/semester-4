abfa0f56b4bb9256feda991019dfa817f6e9d1c5
Swetha S <122204898+swethasakthivel05@users.noreply.github.com>
2024-06-27 16:31:51 +0530

    Add files via upload

---
+++ web projects/projectweb/projectweb/src/main/java/com/example/projectweb/ProjectwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectwebApplication.class, args);
+	}
+
+}
---
+++ web projects/projectweb/projectweb/src/test/java/com/example/projectweb/ProjectwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
